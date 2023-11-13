b09466217fc0b42c835c163ca2b1af35afc7dd43
sfjjor <150743902+sfjjor@users.noreply.github.com>
2023-11-13 16:05:28 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.title('My Parents New Health Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamit.text('Hard-boiled Free-range egg')
+streamlit.text('Hard-boiled Free-range egg')
               
