d110df7ae003b0ed6e242ef74b08c57df86eda3f
sfjjor <150743902+sfjjor@users.noreply.github.com>
2023-11-16 17:06:18 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 # Assign the response of Fruityvice to a dataframe 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Add dataframe to streamlit
